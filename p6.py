7c5abf7fd7afa6fda4826e69381dd5e91885f8d5
adamjolles <adamjolles8@gmail.com>
2023-01-25 19:46:54 -0600

    Update p6.py

--- p6.py
+++ p6.py
@@ -2,7 +2,7 @@ import time
 import random
 
 def main():
-    print("Hello, please enter the start state, the probability of H|H, and the probability of T|T.")
+    print("Hello, welcome to Adam's Mind Reading Machine.")
     while True:
         try:
             state = str(input("Enter the start state: "))
@@ -11,28 +11,28 @@ def main():
             print("That is not a valid option!")
     while True:
         try:
-            a = float(input("Enter the probability of H|H: "))
+            a = int(input("Enter the amount of H -> H transitions for the first 21 flips (0-20): "))
             break
         except:
             print("That is not a valid option!")
     while True:
         try:
-            b = float(input("Enter the probability of T|T: "))
+            b = int(input("Enter the amount of T -> T transitions for the first 21 flips (0-20):  "))
             break
         except:
             print("That is not a valid option!")
 
-    steps = 31
+    steps = 10
     for i in range(steps+1): 
         print(state, end=", ")  
         r = random.random() 
         if state == "H":
-            if r <= a:
+            if r <= (a/20):
                 nextstate = "H"
             else:
                 nextstate = "T"
         elif state == "T":
-            if r <= b:
+            if r <= (b/20):
                 nextstate = "T"
             else:
                 nextstate = "H"
