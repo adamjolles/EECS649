a8cc291c8cde8e3b12461cb54a3c40254d81beb7
adamjolles <59879495+adamjolles@users.noreply.github.com>
2023-01-25 18:51:17 -0600

    Second

--- p6.py
+++ p6.py
@@ -1,14 +1,41 @@
 import time
 import random
 
-def flipCoins(): 
-    heads = 0
-    for i in range(100):
-        if random.random() < 0.5: 
-            heads += 1 
-    return heads
+def main():
+    print("Hello, please enter the start state, the probability of H|H, and the probability of T|T.")
+    while True:
+        try:
+            state = str(input("Enter the start state: "))
+            break
+        except:
+            print("That is not a valid option!")
+    while True:
+        try:
+            a = float(input("Enter the probability of H|H: "))
+            break
+        except:
+            print("That is not a valid option!")
+    while True:
+        try:
+            b = float(input("Enter the probability of T|T: "))
+            break
+        except:
+            print("That is not a valid option!")
 
-def main(): 
-    print("Number of heads: " + str(flipCoins()) + " out of 100") 
+    steps = 31
+    for i in range(steps+1): 
+        print(state, end=", ")  
+        r = random.random() 
+        if state == "H":
+            if r <= a:
+                nextstate = "H"
+            else:
+                nextstate = "T"
+        elif state == "T":
+            if r <= b:
+                nextstate = "T"
+            else:
+                nextstate = "H"
+        state = nextstate
 
 main()
